5083f24823b3d12639b3fdc2820312dc012e059d
Uladzislau Pravalenak <v-epam-UladzislauPra@racetrac.com>
2022-03-21 11:17:29 +0300

    Implement Splash Screen

--- feature/splash/src/main/java/com/vladoleg/plantracker/onboarding/ui/SplashScreen.kt
+++ feature/splash/src/main/java/com/vladoleg/plantracker/onboarding/ui/SplashScreen.kt
@@ -1,16 +1,39 @@
 package com.vladoleg.plantracker.onboarding.ui
 
-import android.window.SplashScreen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.vladoleg.plantracker.coreui.utils.gradientBackground
+import com.vladoleg.plantracker.onboarding.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
-fun SplashScreen() {
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "SPLASH SCREEN")
+fun SplashScreen(toMainScreen: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .gradientBackground(listOf(Color(0xFFff1b49), Color(0xFFe67d20)), 135f),
+        contentAlignment = Alignment.Center
+    ) {
+        val scope = rememberCoroutineScope()
+        Icon(
+            painter = painterResource(id = R.drawable.icon_tree),
+            contentDescription = "Splash icon",
+        )
+
+        SideEffect {
+            scope.launch {
+                delay(3000L)
+                toMainScreen()
+            }
+        }
     }
 }
